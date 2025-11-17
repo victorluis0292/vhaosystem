db39912ef92eec5c7d67f9d46d9f1a738daf3b62
victorluis0292 <vitroidmx@gmail.com>
2025-11-16 23:32:47 -0600

    update 1.7 y leyenda descargar prueba gratis

--- src/main/java/com/vhao/vhao_system/controller/HomeController.java
+++ src/main/java/com/vhao/vhao_system/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    private static final String VERSION = "1.4.6"; /// Cambia esto en cada despliegue
-
+    private static final String VERSION = "1.4.7"; /// Cambia esto en cada despliegue
+//
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("version", VERSION);
